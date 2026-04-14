323aaf3ed82a643f62a52e3c33e6c33e751c9ded
FightAdmin <saas.gymsync@gmail.com>
2026-04-14 10:55:23 -0300

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { registrar, login, hash } from '../controllers/auth.controller.js';
+import { registrar, login } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/registrar', registrar);
 router.post('/login', login);
-router.post('/hash', hash);
 
 export default router;
