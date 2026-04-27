75557339e647fe180b2f95d7efb62c783aa676f9
FightAdmin <saas.gymsync@gmail.com>
2026-04-27 08:16:41 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,33 @@
-import express from 'express';
-import cors from 'cors';
-import authRoutes from './routes/auth.routes.js';
-import alunosRoutes from './routes/alunos.routes.js';
-import dashboardRoutes from "./routes/dashboard.routes.js"
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
+import authRoutes from './routes/auth.routes.js'
+import alunosRoutes from './routes/alunos.routes.js'
+import dashboardRoutes from './routes/dashboard.routes.js'
 
-app.use(cors());
-app.use(express.json());
+const app = express()
+
+// ✅ CORS CORRIGIDO
+app.use(cors({
+  origin: [
+    'http://localhost:5173'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
+
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send('API funcionando 🚀');
-});
+  res.send('API funcionando 🚀')
+})
 
-app.use('/auth', authRoutes);
-app.use('/alunos', alunosRoutes);
+// ROTAS
+app.use('/auth', authRoutes)
+app.use('/alunos', alunosRoutes)
 app.use('/dashboard', dashboardRoutes)
 
+// START
 app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
-});
+  console.log('Servidor rodando na porta 3000')
+})
