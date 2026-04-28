1b6e0f0ea4b869786dc1416b372cc539a91b60ca
FightAdmin <saas.gymsync@gmail.com>
2026-04-27 23:48:16 -0300

    Update dashboard.routes.js

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express"
 import { getDashboard } from "../controllers/dashboard.controller.js"
-import { auth } from "../middlewares/auth.js"
+import { authMiddleware } from "../middlewares/auth.js"
 
 const router = Router()
 
-router.get("/", auth, getDashboard)
+router.get("/dashboard", authMiddleware, getDashboard)
 
 export default router
