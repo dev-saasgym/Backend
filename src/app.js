de7d6cedd766cd9e1b09405ea51254e3443ddef4
FightAdmin <saas.gymsync@gmail.com>
2026-04-28 12:00:26 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,18 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
-app.options('*', cors()) // 🔥 ESSENCIAL
+// 🔥 garante resposta do preflight (ESSENCIAL)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+
+  next()
+})
 
 app.use(express.json())
 
