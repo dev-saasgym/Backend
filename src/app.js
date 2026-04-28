792ef184afc4667fb673dc8d3bd1855ada9f62ee
FightAdmin <saas.gymsync@gmail.com>
2026-04-28 08:04:14 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -28,6 +28,8 @@ app.use('/auth', authRoutes)
 app.use('/alunos', alunosRoutes)
 app.use('/dashboard', dashboardRoutes)
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000')
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
