5fbbd35dd83671caf1f9ddcc0491c691145e5c53
FightAdmin <saas.gymsync@gmail.com>
2026-04-28 17:42:05 -0300

    Change root response message to 'API funcionando 🚀'
    Update root response message to indicate API is working.

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.json({ status: 'ok' })
+  res.send('API funcionando 🚀')
 })
 
 app.use('/auth', authRoutes)
