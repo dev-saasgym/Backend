b045aa6cf5e9a9840a23649cad13d5e032370c4f
FightAdmin <saas.gymsync@gmail.com>
2026-04-28 14:48:34 -0300

    Refactor authMiddleware for better error handling

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,18 +1,26 @@
 import jwt from 'jsonwebtoken';
 
 export function authMiddleware(req, res, next) {
-  const token = req.headers.authorization?.split(' ')[1];
-
-  if (!token) return res.status(401).json({ error: 'Token não fornecido' });
-
   try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({ error: 'Token não fornecido' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Token inválido' });
+    }
+
     const decoded = jwt.verify(token, 'segredo');
 
-    req.user = decoded; 
-    // { userId, academia_id }
+    req.user = decoded;
 
     next();
-  } catch {
+  } catch (err) {
+    console.error(err);
     return res.status(401).json({ error: 'Token inválido' });
   }
 }
